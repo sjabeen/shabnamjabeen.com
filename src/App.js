1c988544b3662de5d9c0061a0bbd870a39c571dd
sjabeen <jabeen@umd.edu>
2023-04-30 20:48:53 -0400

    carousel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import PaintingCard from './card/PaintingCard.tsx';
+import paintings from "./static/paintings.json";
+import { Stack } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img width={400} height={600} src={"/images/KalleenSabrina.jpg"}/>
-        <p>
-          Type "npm run start" in the home directory to run locally, git commit to push to AWS Amplify. Store any static images you want in the /public folder and reference them by their path in that folder (e.g. /public/img/painting.jpg becomes /img/painting.jpg). Enjoy.
-        </p>
+        <Stack direction="row" columnGap="5vw" sx={{ width: "80vw", overflowX: "auto" }}>
+        {paintings.map(p => (<PaintingCard 
+          painting={p.painting}
+          name={p.name} date={p.date} 
+          desc={p.description} 
+          longDesc={p.longDescription} />))}
+        </Stack>
       </header>
     </div>
   );
